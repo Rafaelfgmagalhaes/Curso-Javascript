7047028b01d12433b53943edda7308591aa24aef
Rafaelfgmagalhaes <rafaelfgmagalhaes@gmail.com>
2022-09-27 15:51:24 -0300

    exercicios de javascript

---
+++ ex002/app.js
@@ -0,0 +1,3 @@
+var nome = prompt('Qual o seu nome?');
+document.write(`olá, <strong>${nome}</strong>! seu nome tem ${nome.length} letras.<br/>`);
+document.write(`Seu nome em maiúsculas é ${nome.toUpperCase()}`);
---
+++ ex003/app.js
@@ -0,0 +1,13 @@
+var p1 = window.document.getElementsByTagName('p')[1]
+window.document.write('Está escrito assim: ' + p1.innerText)
+p1.style.color ='blue'
+
+var corpo = window.document.body
+corpo.style.color = 'yellow'
+
+var d = window.document.getElementById('msg')
+d.style.background = 'green'
+
+var e = document.querySelector('h1#teste')
+e.style.background = 'gray'
+

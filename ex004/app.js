9e3c648b5beb3b4d657e7ba3b1e456984ddab525
Rafaelfgmagalhaes <rafaelfgmagalhaes@gmail.com>
2022-09-27 20:39:12 -0300

    exercicios javascript

---
+++ ex004/app.js
@@ -0,0 +1,18 @@
+function clicar(){
+    var a = document.getElementById('area')
+    a.innerText = 'clicou!';
+    a.style.background = 'red';
+}
+
+function entrar(){
+    var a = document.getElementById('area')
+    a.innerText = 'entrou!';
+    a.style.background = 'blue';
+}
+
+function sair(){
+    var a = document.getElementById('area')
+    a.innerText = 'saiu!';
+    a.style.background = 'yellow';
+    a.style.color = 'black';
+}

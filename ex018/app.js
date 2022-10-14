f1e5ac077e330632ca0613239990b5660375c9b6
Rafaelfgmagalhaes <rafaelfgmagalhaes@gmail.com>
2022-10-13 21:16:28 -0300

    array, e funções

---
+++ ex018/app.js
@@ -0,0 +1,6 @@
+let num = [5,8,4] // declaração do vetor
+console.log(`O nosso vetor é ${num}`)
+num.push(7) // adicionando valor ao vetor
+console.log(num) //mostra o vetor
+num.length // mostra o tamanho do vetor
+num.sort() // coloca os valores do vetor em ordem
---
+++ ex018/vetor-na-tela.js
@@ -0,0 +1,18 @@
+let valores =[8,1,7,4,2,9]
+
+for(let pos=0; pos < valores.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+
+// simplificando o codigo:
+
+/*
+
+for(let pos in valores){
+    console.log(valores[pos])
+}
+
+*/
+
+valores.indexOf(7) /*Retorna a chave onde o valor se encontra, caso o valor nao exista no vetor o javascript retorna o valor -1 */
+ 
